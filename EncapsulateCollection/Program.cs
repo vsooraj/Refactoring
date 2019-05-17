d257e1620e56e9533e3a06ae12268173413cc4a0
Sooraj V <sooraj.v@cabotsolutions.com>
2019-05-17 16:16:04 +0530

    Add project files.

---
+++ EncapsulateCollection/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EncapsulateCollection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
